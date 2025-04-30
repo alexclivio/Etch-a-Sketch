b72b11294a6bb1821c14aefc5a7bb47e59679f0a
Alex Clivio <amrclivio@gmail.com>
2025-04-30 00:31:06 -0700

    add functions to handle logic, add button to generate grid, refactor logic to dynamically set square width

--- index.js
+++ index.js
@@ -1,23 +1,50 @@
 console.log("Success")
 
+const gridButton = document.getElementById("grid-button")
 const gridContainer = document.getElementById("grid-container");
 
-for (let i = 0; i < 256; i++) {
-  const gridSquare = document.createElement("div");
-  gridSquare.classList.add("grid-square");
-  gridContainer.append(gridSquare);
+gridButton.addEventListener("click", (e) => {
+  const allDivs = document.querySelectorAll(".grid-square");
+
+  allDivs.forEach(div => {
+    div.remove();
+  });
+
+  let gridResponse = prompt("Please enter grid size.")
+
+  generateGrid(gridResponse)
+})
+
+const generateGrid = (num) => {
+  const gridNum = num * num
+  const perRow = Math.sqrt(gridNum);
+  console.log(perRow)
+  const itemWidth = 100 / perRow;
+  for (let i = 0; i < gridNum; i++) {
+    const gridSquare = document.createElement("div");
+    gridSquare.style.width = `${itemWidth}%`;
+    gridSquare.classList.add("grid-square");
+    gridContainer.append(gridSquare);
+  }
+
+  divHover()
 }
 
-const innerDivs = document.querySelectorAll(".grid-square");
+const divHover = () => {
+  const innerDivs = document.querySelectorAll(".grid-square");
 
-innerDivs.forEach((div) => {
-  div.addEventListener("mouseover", (e) => {
-    e.target.classList.add("hover-in");
+  console.log(innerDivs)
+  
+  innerDivs.forEach((div) => {
+    div.addEventListener("mouseover", (e) => {
+      e.target.classList.add("hover-in");
+    });
   });
-});
-
-innerDivs.forEach((div) => {
-  div.addEventListener("mouseleave", (e) => {
-    e.target.classList.remove("hover-in")
+  
+  innerDivs.forEach((div) => {
+    div.addEventListener("mouseleave", (e) => {
+      e.target.classList.remove("hover-in")
+    })
   })
-})
+}
+
